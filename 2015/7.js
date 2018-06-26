da4c4d9b73332a7d6c3c4984282ce93116125863
Stepan Legachev <siberex@gmail.com>
2018-06-26 20:37:58 +0300

    adding solution for 2015/7

--- 2015/7.js
+++ 2015/7.js
@@ -11,9 +11,35 @@ process.on('unhandledRejection', error => {
 (async function () {
     // If current file is 123.js, will read file 123.txt as input
     const inputFile = __dirname + '/' + path.basename(__filename, '.js') + '.txt';
+    const outputFile = __dirname + '/' + path.basename(__filename, '.js') + '-out.txt';
     let data = fs.readFileSync(inputFile, 'utf8');
 
-    console.log(data);
-    // ...
+    data = data.split("\n");
+
+    let testIndex = 1;
+    console.log(data[testIndex]);
+
+    let matchRe = /^(.+) -> (.+)$/;
+    let matchReCmd = /^(\w+)? ?(AND|OR|NOT|LSHIFT|RSHIFT)? ?(\w+)$/;
+
+    // Parse commands
+    data = data.map(line => {
+        // line = 'hz RSHIFT 1 -> is'
+        let res = matchRe.exec(line);
+        if (res) res = res.slice(1);
+        return res;
+    });
+
+    //console.log(data[testIndex]);
+    //console.log(matchReCmd.exec(data[testIndex][0]).slice(1));
+
+    let parsed = data.map(cmd => {
+        let res = matchReCmd.exec(cmd[0]);
+        if (res) res = res.slice(1);
+        return res;
+    });
+
+    console.log(parsed[testIndex]);
+
 
 })();
