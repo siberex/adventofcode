9cf5e1dabe3769e3a45f119fc27be434aab91745
Stepan Legachev <siberex@gmail.com>
2018-06-28 06:32:06 +0300

    adding solution for 2015/7

--- 2015/7.js
+++ 2015/7.js
@@ -17,29 +17,112 @@ process.on('unhandledRejection', error => {
     data = data.split("\n");
 
     let testIndex = 1;
-    console.log(data[testIndex]);
+    //console.log(data[testIndex]);
 
-    let matchRe = /^(.+) -> (.+)$/;
-    let matchReCmd = /^(\w+)? ?(AND|OR|NOT|LSHIFT|RSHIFT)? ?(\w+)$/;
+    let matchRe = /^(.+) -> ([a-z]+)$/;
+    let matchReCmd = {
+        'and'       : /^([a-z]+|\d+) AND ([a-z]+|\d+)$/,
+        'or'        : /^([a-z]+|\d+) OR ([a-z]+|\d+)$/,
+        'not'       : /^NOT ([a-z]+)$/,
+        'lshift'    : /^([a-z]+) LSHIFT (\d+)$/,
+        'rshift'    : /^([a-z]+) RSHIFT (\d+)$/,
+        'val'       : /^([a-z]+|\d+)$/,
+    };
+
+
+
+
+    let wires = {};
 
     // Parse commands
     data = data.map(line => {
         // line = 'hz RSHIFT 1 -> is'
         let res = matchRe.exec(line);
-        if (res) res = res.slice(1);
-        return res;
+        // res = ['hz RSHIFT 1', 'is']
+        if (!res) {
+            console.error('NOT PARSED: ' + line);
+            return null;
+        }
+
+        res = res.slice(1);
+        let [commands, wire] = res;
+
+        for (let cmd in matchReCmd) {
+            let re = matchReCmd[cmd];
+
+            let values = re.exec(commands);
+            if (values) {
+                values = values.slice(1);
+
+                if (cmd === 'val') {
+                    wires[wire] = values[0];
+                    // console.log(wires[wire]);
+                } else {
+                    // wires['is'] = ['rshift', 'hz', 1]
+                    wires[wire] = [cmd, ...values];
+                }
+                break;
+            }
+        }
+
+        if (!wires[wire]) {
+            console.error('NOT PARSED: ' + line);
+        }
+
+        //wires[wire] = commands;
+        return wires[wire];
     });
 
-    //console.log(data[testIndex]);
-    //console.log(matchReCmd.exec(data[testIndex][0]).slice(1));
+    console.log( 'Wires count: ' + Object.keys(wires).length );
 
-    let parsed = data.map(cmd => {
-        let res = matchReCmd.exec(cmd[0]);
-        if (res) res = res.slice(1);
-        return res;
-    });
 
-    console.log(parsed[testIndex]);
+    function applyCmd(cmd, arg1, arg2) {
+        if (typeof arg1 !== 'undefined' && isNaN(arg1)) {
+            arg1 = applyWiring(arg1);
+        } else {
+            arg1 = parseInt(arg1);
+        }
+        if (typeof arg2 !== 'undefined' && isNaN(arg2)) {
+            arg2 = applyWiring(arg2);
+        } else {
+            arg2 = parseInt(arg2);
+        }
 
+        switch (cmd) {
+            case 'and':
+                return arg1 & arg2;
+            case 'or':
+                return arg1 | arg2;
+            case 'not':
+                return ~arg1;
+            case 'lshift':
+                return arg1 << arg2;
+            case 'rshift':
+                return arg1 >> arg2;
+        }
+    }
+
+    function applyWiring(wireName) {
+        let wire = wires[wireName];
+
+        if (typeof wire === 'string') {
+            if (isNaN(wire)) {
+                wires[wireName] = applyWiring(wire);
+            } else {
+                wires[wireName] = parseInt(wire);
+            }
+        }
+
+        if ( Array.isArray(wire) ) {
+            let [cmd, ...params] = wire;
+            wires[wireName] = applyCmd(cmd, ...params);
+        }
+
+        return wires[wireName];
+    } // applyWiring
+
+    let result = applyWiring('a');
+
+    console.log(result);
 
 })();
