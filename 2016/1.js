06adaadff0718b0deaa2cf02e5a803c1e03170d9
Stepan Legachev <siberex@gmail.com>
2018-06-30 01:10:30 +0300

    adding solution for 2016/1

---
+++ 2016/1.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const fs    = require('fs')
+    , path  = require('path')
+;
+
+process.on('unhandledRejection', error => {
+    console.error('unhandledRejection', error.message);
+});
+
+(async function () {
+    // If current file is 123.js, will read file 123.txt as input
+    const inputFile = __dirname + '/' + path.basename(__filename, '.js') + '.txt';
+    const outputFile = __dirname + '/' + path.basename(__filename, '.js') + '-out.txt';
+    let data = fs.readFileSync(inputFile, 'utf8');
+
+    console.log(data);
+    // ...
+
+})();
