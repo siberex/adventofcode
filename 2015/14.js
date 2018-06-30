3bfafd8c17e4d30fc167d0caeb5df67febc1cc78
Stepan Legachev <siberex@gmail.com>
2018-06-30 20:04:12 +0300

    added puzzle inputs

---
+++ 2015/14.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const fs    = require('fs')
+    , path  = require('path')
+;
+
+process.on('unhandledRejection', error => {
+    console.error('unhandledRejection', error.message);
+});
+
+(async function () {
+    // If current file is 123.js, will read file 123.txt as input
+    const inputFile = __dirname + '/' + path.basename(__filename, '.js') + '.txt';
+    const outputFile = __dirname + '/' + path.basename(__filename, '.js') + '-out.txt';
+    let data = fs.readFileSync(inputFile, 'utf8');
+    data = data.split("\n");
+
+
+    data.map(line => {
+
+        console.log(line);
+        // ...
+
+    });
+
+    // ...
+
+})();
