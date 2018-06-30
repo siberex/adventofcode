9e91f7c1612838e2c1e3c3586df16ed277e97538
Stepan Legachev <siberex@gmail.com>
2018-06-30 06:32:56 +0300

    adding solution for 2015/13

--- 2015/13.js
+++ 2015/13.js
@@ -2,6 +2,7 @@
 
 const fs    = require('fs')
     , path  = require('path')
+    , Graph = require('../helpers').Graph
 ;
 
 process.on('unhandledRejection', error => {
@@ -13,8 +14,33 @@ process.on('unhandledRejection', error => {
     const inputFile = __dirname + '/' + path.basename(__filename, '.js') + '.txt';
     const outputFile = __dirname + '/' + path.basename(__filename, '.js') + '-out.txt';
     let data = fs.readFileSync(inputFile, 'utf8');
+    data = data.split("\n");
 
-    console.log(data);
-    // ...
+
+    let persons = new Graph();
+
+    let matchRe = /^(\w+) would (lose|gain) (\d+) happiness units by sitting next to (\w+)\.$/;
+
+    data.map(line => {
+        // line = 'Carol would lose 62 happiness units by sitting next to Alice.'
+
+        let res = matchRe.exec(line);
+        if (!res) {
+            console.error('NOT PARSED: ' + line);
+            return null;
+        }
+
+        // res = [ 'Carol', 'lose', '62', 'Alice' ]
+        res = res.slice(1);
+
+        let [a, sign, units, b] = res;
+        units = parseInt(sign === 'lose' ? '-' + units : units);
+
+        // ['Carol', 'Alice', -62]
+        persons.addEdge(a, b, units);
+        return [a, b, units];
+    });
+
+
 
 })();
