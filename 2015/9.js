59cf79cec384223661acae4c3933232142b6f30b
Stepan Legachev <siberex@gmail.com>
2018-06-30 05:38:59 +0300

    moved Graph implementation to helpers

--- 2015/9.js
+++ 2015/9.js
@@ -2,134 +2,12 @@
 
 const fs    = require('fs')
     , path  = require('path')
+    , Graph = require('../helpers').Graph
 ;
 
 process.on('unhandledRejection', error => {
     console.error('unhandledRejection', error.message);
 });
-
-
-class Graph {
-    constructor() {
-        this.vertices = new Set();
-        this.edges = new Map();
-    }
-
-    addVertex(name) {
-        if ( !this.vertices.has(name) ) {
-            this.vertices.add(name);
-        }
-        return this.vertices.size;
-    }
-
-    deleteVertex(name) {
-        if ( this.vertices.has(name) ) {
-            this.vertices.delete(name);
-
-            if ( this.edges.has(name) ) {
-                this.edges.delete(name);
-            }
-
-            this.deleteDirectedEdge(null, name);
-        }
-        return this.vertices.size;
-    }
-
-    addEdge(from, to, weight) {
-        this.addDirectedEdge(from, to, weight);
-        this.addDirectedEdge(to, from, weight);
-    } // addEdge
-
-    deleteEdge(from, to) {
-        this.deleteDirectedEdge(from, to);
-        this.deleteDirectedEdge(to, from);
-    } // deleteEdge
-
-    addDirectedEdge(from, to, weight = Infinity) {
-        if ( !this.vertices.has(from) ) {
-            this.addVertex(from);
-        }
-        if ( !this.vertices.has(to) ) {
-            this.addVertex(to);
-        }
-
-        if ( !this.edges.has(from) ) {
-            this.edges.set(from, []);
-        }
-        this.edges.get(from).push({
-            to      : to,
-            weight  : weight
-        });
-    } // addDirectedEdge
-
-    deleteDirectedEdge(from = null, to) {
-        let fromVertices = this.vertices;
-
-        if (from !== null) {
-            fromVertices = [from];
-        }
-
-        for (let fromVertex of fromVertices) {
-            if ( this.edges.has(fromVertex) ) {
-                this.edges.set(
-                    fromVertex,
-                    this.edges.get(fromVertex)
-                        .filter(edge => edge.to !== to)
-                );
-            }
-        }
-    } // deleteDirectedEdge
-
-    getNearest(from) {
-        let nearest = this.edges.get(from).reduce((acc, val) => {
-            if (acc === null) {
-                return val;
-            }
-
-            if (acc.weight > val.weight) {
-                return val;
-            }
-
-            return acc;
-        }, null);
-
-        if (nearest === null) {
-            nearest = {to: null, weight: Infinity};
-        }
-
-        return nearest;
-    } // getNearest
-
-    getFurthest(from) {
-        let furthest = this.edges.get(from).reduce((acc, val) => {
-            if (acc === null) {
-                return val;
-            }
-
-            if (acc.weight < val.weight) {
-                return val;
-            }
-
-            return acc;
-        }, null);
-
-        if (furthest === null) {
-            furthest = {to: null, weight: Infinity};
-        }
-
-        return furthest;
-    } // getFurthest
-
-    clone() {
-        let graph = new this.constructor();
-        graph.vertices = new Set(this.vertices);
-        graph.edges = new Map(this.edges);
-        return graph;
-    } // clone
-
-} // Graph
-
-
 
 (async function () {
     // If current file is 123.js, will read file 123.txt as input
@@ -143,7 +21,7 @@ class Graph {
 
     let matchRe = /^(.+) to (.+) = (\d+)$/;
 
-    data = data.map(line => {
+    data.map(line => {
         // line = 'AlphaCentauri to Straylight = 34'
 
         let res = matchRe.exec(line);
@@ -163,7 +41,6 @@ class Graph {
         return res;
     });
 
-    //console.log(data[2]);
 
     // Part 1
     let distances = [];
--- helpers.js
+++ helpers.js
@@ -54,4 +54,126 @@ function group(input, isGroup = null) {
 } // group
 
 
+class Graph {
+    constructor() {
+        this.vertices = new Set();
+        this.edges = new Map();
+    }
+
+    addVertex(name) {
+        if ( !this.vertices.has(name) ) {
+            this.vertices.add(name);
+        }
+        return this.vertices.size;
+    }
+
+    deleteVertex(name) {
+        if ( this.vertices.has(name) ) {
+            this.vertices.delete(name);
+
+            if ( this.edges.has(name) ) {
+                this.edges.delete(name);
+            }
+
+            this.deleteDirectedEdge(null, name);
+        }
+        return this.vertices.size;
+    }
+
+    addEdge(from, to, weight) {
+        this.addDirectedEdge(from, to, weight);
+        this.addDirectedEdge(to, from, weight);
+    } // addEdge
+
+    deleteEdge(from, to) {
+        this.deleteDirectedEdge(from, to);
+        this.deleteDirectedEdge(to, from);
+    } // deleteEdge
+
+    addDirectedEdge(from, to, weight = Infinity) {
+        if ( !this.vertices.has(from) ) {
+            this.addVertex(from);
+        }
+        if ( !this.vertices.has(to) ) {
+            this.addVertex(to);
+        }
+
+        if ( !this.edges.has(from) ) {
+            this.edges.set(from, []);
+        }
+        this.edges.get(from).push({
+            to      : to,
+            weight  : weight
+        });
+    } // addDirectedEdge
+
+    deleteDirectedEdge(from = null, to) {
+        let fromVertices = this.vertices;
+
+        if (from !== null) {
+            fromVertices = [from];
+        }
+
+        for (let fromVertex of fromVertices) {
+            if ( this.edges.has(fromVertex) ) {
+                this.edges.set(
+                    fromVertex,
+                    this.edges.get(fromVertex)
+                        .filter(edge => edge.to !== to)
+                );
+            }
+        }
+    } // deleteDirectedEdge
+
+    getNearest(from) {
+        let nearest = this.edges.get(from).reduce((acc, val) => {
+            if (acc === null) {
+                return val;
+            }
+
+            if (acc.weight > val.weight) {
+                return val;
+            }
+
+            return acc;
+        }, null);
+
+        if (nearest === null) {
+            nearest = {to: null, weight: Infinity};
+        }
+
+        return nearest;
+    } // getNearest
+
+    getFurthest(from) {
+        let furthest = this.edges.get(from).reduce((acc, val) => {
+            if (acc === null) {
+                return val;
+            }
+
+            if (acc.weight < val.weight) {
+                return val;
+            }
+
+            return acc;
+        }, null);
+
+        if (furthest === null) {
+            furthest = {to: null, weight: Infinity};
+        }
+
+        return furthest;
+    } // getFurthest
+
+    clone() {
+        let graph = new this.constructor();
+        graph.vertices = new Set(this.vertices);
+        graph.edges = new Map(this.edges);
+        return graph;
+    } // clone
+
+} // Graph
+
+
 module.exports.group = group;
+module.exports.Graph = Graph;
