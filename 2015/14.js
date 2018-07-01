80539c075f292dad3cade1a1f248d3bd53782e6c
Stepan Legachev <siberex@gmail.com>
2018-07-01 03:27:02 +0300

    added solution for 2015/14

--- 2015/14.js
+++ 2015/14.js
@@ -11,18 +11,65 @@ process.on('unhandledRejection', error => {
 (async function () {
     // If current file is 123.js, will read file 123.txt as input
     const inputFile = __dirname + '/' + path.basename(__filename, '.js') + '.txt';
-    const outputFile = __dirname + '/' + path.basename(__filename, '.js') + '-out.txt';
     let data = fs.readFileSync(inputFile, 'utf8');
     data = data.split("\n");
 
 
-    data.map(line => {
+    let time = 2503;
 
-        console.log(line);
-        // ...
 
+    let matchRe = /^(\w+) can fly (\d+) km\/s for (\d+) seconds, but then must rest for (\d+) seconds\.$/;
+
+    data = data.map(line => {
+        // line = 'Donner can fly 9 km/s for 5 seconds, but then must rest for 38 seconds.'
+
+        if (!line.length) {
+            return null;
+        }
+
+        let res = matchRe.exec(line);
+        if (!res) {
+            console.error('NOT PARSED: ' + line);
+            return null;
+        }
+
+        // res = [ 'Donner', '9', '5', '38' ]
+        res = res.slice(1);
+
+        let [name, speed, timeFly, timeRest] = res;
+        speed       = speed|0;
+        timeFly     = timeFly|0;
+        timeRest    = timeRest|0;
+
+        return [name, speed, timeFly, timeRest];
+    }).filter(v => v !== null);
+
+
+    let firstRun = data.map(deer => {
+        let [name, speed, timeFly, timeRest] = deer;
+        // console.log([name, speed, timeFly, timeRest]);
+
+        let segmentDistance = speed * timeFly;
+        let segmentTime = timeFly + timeRest;
+
+        let dist = Math.floor(time / segmentTime) * segmentDistance;
+        dist += Math.min(time % segmentTime, timeFly) * speed;
+
+        //console.log(dist);
+        return dist;
     });
 
-    // ...
+    console.log(
+        firstRun.reduce( (acc, val) => Math.max(acc, val), -Infinity ),
+        'Part 1'
+    );
+
+
+    for (let s = 1; s <= time; s++) {
+
+
+
+    }
+
 
 })();
