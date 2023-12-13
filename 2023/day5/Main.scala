2d92c72304e251e55cb72e0411a17dca685e1e39
Stepan Legachev <gitlog@sib.li>
2023-12-13 16:19:50 +0300

    add naive solution for 2023.5.2

--- 2023/day5/Main.scala
+++ 2023/day5/Main.scala
@@ -1,6 +1,8 @@
+import scala.collection.immutable.NumericRange
 import scala.io.Source
 import scala.util.matching.Regex
 import java.io.{FileNotFoundException, IOException}
+import scala.language.strictEquality
 
 // val inputFilename = "2023/day5/input.sample.txt"
 val inputFilename = "2023/day5/input.txt"
@@ -32,10 +34,10 @@ type MapGroup = {
     val maps: List[Mapping]
 }
 */
-class MapGroup(val name: String, val maps: List[Mapping]):
+class MapGroup(val name: String, val maps: LazyList[Mapping]):
 
     def doMapping(mapFrom: Long): Long =
-        val mappingRes = maps.to(LazyList).map(
+        val mappingRes = maps.map(
                 m => m.doMapping(mapFrom)
             ).collectFirst {
                 case Some(d) => d 
@@ -54,7 +56,7 @@ object MapGroup:
     def apply(name: String, maps: List[List[Long]]): MapGroup =
         new MapGroup(
             name,
-            maps.map(l => Mapping(l))
+            maps.map(l => Mapping(l)).to(LazyList)
         )
 end MapGroup
 
@@ -90,14 +92,33 @@ soil-mapTo-fertilizer map:
 */
 val reMappings: Regex = raw"([a-z-]+) map:\n((?:\d+ ?[\n]?)+)+".r
 
-// Helper to convert "37 52 2" to (37, 52, 72)
-val toLongList = (str: String) => str.split(' ').map(_.trim).map(_.toLong).toList
-
 def parseMappings(name: String, rawValue: String): MapGroup =
     // rawValue = "0 15 37\n37 52 2\n39 0 15"
     // mappingsList = ( (0,15,37), (37,52,2), (39,0,15) )
     val mappingsList = rawValue.split("\n").map(_.trim).toList.map(toLongList)
     return MapGroup(name, mappingsList)
+
+
+// Helper to convert "37 52 2" to (37, 52, 72)
+val toLongList = (str: String) => str.split(' ').map(_.trim).map(_.toLong).toList
+
+/**
+ * Collapse list of numeric ranges:
+ * mergeRanges( List(100 to 200, 70 to 75, 2 to 50, 80 to 85, 75 to 90, 1 to 45, 3 to 40) ) == List(1 to 50, 70 to 90, 100 to 200)
+ *
+ * Kudos to Safta Catalin Mihai:
+ * https://gist.github.com/saftacatalinmihai/be429faaccbc8b71335561a31df2b994
+ */
+type Interval = NumericRange[Long]
+def mergeRanges(intervals: List[Interval]): List[Interval] = mergeRanges(intervals.sortBy(_.start), List())
+def mergeRanges(intervals: List[Interval],
+                    acc: List[Interval]): List[Interval] = intervals match {
+    case a :: b :: rest if a.end >= b.start => mergeRanges((a.start to Math.max(a.end, b.end)) :: rest, acc)
+    case a :: b :: rest                 => mergeRanges(b :: rest, acc :+ a)
+    case a :: Nil                       => acc :+ a
+    case _                              => acc
+}
+
 
 // Main
 def parseInput(input: String): Unit =
@@ -114,8 +135,28 @@ def parseInput(input: String): Unit =
     // println(seeds)
     // println(seedMappings)
 
-    val result = seedMappings.min 
-    println(result)
+    val resultPart1 = seedMappings.min
+    println(s"Part1: $resultPart1")
+
+    // Part 2: Treat Seeds input as ranges of (start, len) pairs
+    var resultPart2 = seedMappings(0);
+    
+    // If needed, ranges could be merged with mergeRanges(seedRanges) — not needed for the input though
+    val seedRanges = seeds.sliding(2, 2).toList.map(l => l(0) until (l(0) + l(1))).sortBy(_.start)
+    
+    // Naive and extremely slow solution
+    // Proper one would be to map ranges (splitting source range)
+    seedRanges.foreach(range => {
+        range.foreach(seed => {
+            
+            val res = getMappping(seed, mapGroups)
+            if (res < resultPart2) {
+                resultPart2 = res
+            }
+        })
+    })
+
+    println(s"Part2: $resultPart2")
 
     return
 
